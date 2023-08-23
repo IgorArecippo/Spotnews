1de5a9c5cd0d822739a1ee10794904888c2962e0
Igor Arecippo <iarecippo@gmail.com>
2023-08-23 15:40:14 -0300

    feat: adicionaos templates: base.html e home.html

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from news.models import Categories, News, Users
 
-# Register your models here.
+admin.site.register(Categories)
+admin.site.register(News)
+admin.site.register(Users)
