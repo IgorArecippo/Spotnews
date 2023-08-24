21ca0ca04b0f5c470cb3e51fba293416dab1735d
Igor Arecippo <iarecippo@gmail.com>
2023-08-24 14:47:08 -0300

    feat: add categories form html

---
+++ news/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from news.models.category_model import Categories
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Categories
+        fields = ['name']
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from news.models import News
+from news.forms import CategoryForm
 
 
 def news_details(request, id):
@@ -12,3 +13,16 @@ def home(request):
     news = News.objects.all()
     context = {'news': news}
     return render(request, 'home.html', context)
+
+
+def categories_form(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home-page')
+    else:
+        form = CategoryForm()
+
+    context = {'form': form}
+    return render(request, 'categories_form.html', context)
