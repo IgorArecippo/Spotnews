ca4b1dc7d91665457ff1739da49f7c5c914674ff
Igor Arecippo <iarecippo@gmail.com>
2023-08-24 15:01:57 -0300

    feat: add api/categories url

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from news.views import home, news_details, categories_form
+from rest_framework.routers import DefaultRouter
+from news_rest.views.categories_view import CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
     path('', home, name='home-page'),
     path('news/<int:id>/', news_details, name='news-details-page'),
     path('categories/', categories_form, name='categories-form'),
+    path('api/', include(router.urls)),
 ]
---
+++ news_rest/serializers/categories_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from news.models.category_model import Categories
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields = ['id', 'name']
