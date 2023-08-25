5102e02940843a29e94a41901c8511a035af2ffc
Igor Arecippo <iarecippo@gmail.com>
2023-08-25 08:16:38 -0300

    add route api news

--- news/urls.py
+++ news/urls.py
@@ -3,15 +3,17 @@ from news.views import home, news_details, categories_form
 from rest_framework.routers import DefaultRouter
 from news_rest.views.categories_view import CategoryViewSet
 from news_rest.views.users_view import UserViewSet
+from news_rest.views.news_view import NewsViewSet
+
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'users', UserViewSet)
+router.register(r'news', NewsViewSet)
 
 urlpatterns = [
     path('', home, name='home-page'),
     path('news/<int:id>/', news_details, name='news-details-page'),
     path('categories/', categories_form, name='categories-form'),
-    path('api/', include(router.urls)),
     path('api/', include(router.urls)),
 ]
---
+++ news_rest/serializers/news_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from news.models.news_model import News
+
+
+class NewsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id',
+                  'title',
+                  'content', 'author', 'created_at', 'image', 'categories']
